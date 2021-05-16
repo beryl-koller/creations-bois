cfc81adb3b85f838363af8972aef1d60efb57ef5
Beryl Armand <beryl.armand@gmail.com>
2021-05-16 09:58:48 +0200

    End of session 64

---
+++ src/Form/ResetPasswordType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class ResetPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('new_password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe et la confirmation doivent être identiques.',
+                'required' => true,
+                'first_options' => [ 'label' => 'Mon nouveau mot de passe'],
+                'second_options' => [ 'label' => 'Confirmation de mon nouveau mot de passe']
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Modifier mon mot de passe'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
